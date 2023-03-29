c8314f7be4543cd63b2aac24433a1b2f5653c7a5
Ivan Toselli <ivan.toselli31@gmail.com>
2023-03-29 11:52:09 +0200

    UlpoadSite001

---
+++ src/app/ivztoz8/ivztoz8.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ivztoz8',
+  templateUrl: './ivztoz8.component.html',
+  styleUrls: ['./ivztoz8.component.css']
+})
+export class Ivztoz8Component {
+
+}
